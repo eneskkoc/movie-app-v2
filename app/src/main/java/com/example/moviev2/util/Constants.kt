1c43287c7a6d36351c40b20140f47fd05c2a1dc7
eneskkoc <eneskkoc@hotmail.com>
2022-01-24 18:05:01 +0300

    Update Constants.kt

--- app/src/main/java/com/example/moviev2/util/Constants.kt
+++ app/src/main/java/com/example/moviev2/util/Constants.kt
@@ -2,7 +2,7 @@ package com.example.moviev2.util
 
 object Constants {
     const val API_BASE_PATH = "https://api.themoviedb.org/3/movie/"
-    const val API_KEY = "1f41197ea9884f660621c446b91ebaba"
+    const val API_KEY = "https://api.themoviedb.org get api key"
     const val Languae = "en-US"
 
 }
